3cde2a9033163eb1fb6cb73f011c35354820beca
@siroinaba <siroinaba@gmail.com>
2021-12-25 17:14:53 +0900

    色々頑張ってる

---
+++ Assets/ClickManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = new Ray();
+            RaycastHit hit = new RaycastHit();
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            //マウスクリックした場所からRayを飛ばし、オブジェクトがあればtrue 
+            if (Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity))
+            {
+                if (hit.collider.gameObject.CompareTag("Tadashi"))
+                {
+                    Debug.Log("ADSFASFADSFDSAFDSAFDAFDSAFADSF");
+                }
+            }
+        }
+    }
+}
