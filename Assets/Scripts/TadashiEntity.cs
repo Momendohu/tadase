9d2c598a76c4b5a162a88f76aaee8e0b8be41f9b
@siroinaba <siroinaba@gmail.com>
2021-12-25 17:20:00 +0900

    色々頑張ってる

---
+++ Assets/Scripts/TadashiEntity.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TadashiEntity : MonoBehaviour
+{
+    public int uniqueId { get; set; }
+    public int pictId { get; set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClick()
+    {
+
+    }
+
+    public void ChangeButtonImage()
+    {
+        
+    }
+}
